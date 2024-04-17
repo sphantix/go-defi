4fcd7511795253b090670d620aa0c4a5bcb5aaad
sphantix <sphantix@gmail.com>
2024-04-17 11:08:42 +0800

    feat: add some methods

--- uniswapv2/client.go
+++ uniswapv2/client.go
@@ -67,6 +67,75 @@ func (c *Client) GetReserves(token0, token1 common.Address) (*Reserve, error) {
 	return &Reserve{Reserve0: reserves.Reserve0, Reserve1: reserves.Reserve1, BlockTimestampLast: reserves.BlockTimestampLast}, nil
 }
 
+// GetReserves retursn the available reserves in a pair
+func (c *Client) GetReserveFromPair(addr common.Address) (*Reserve, error) {
+	//addr := GeneratePairAddress(token0, token1)
+	caller, err := uniswapv2pair.NewUniswapv2pairCaller(addr, c.bc)
+	if err != nil {
+		return nil, errors.Wrap(err, "new uniswap pair caller")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	reserves, err := caller.GetReserves(&bind.CallOpts{
+		Context: ctx,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "get reserves")
+	}
+	return &Reserve{Reserve0: reserves.Reserve0, Reserve1: reserves.Reserve1, BlockTimestampLast: reserves.BlockTimestampLast}, nil
+}
+
+// GetReserves retursn the available reserves in a pair
+func (c *Client) GetReservesInBlock(token0, token1 common.Address, bn int64) (*Reserve, error) {
+	addr := GeneratePairAddress(token0, token1)
+	caller, err := uniswapv2pair.NewUniswapv2pairCaller(addr, c.bc)
+	if err != nil {
+		return nil, errors.Wrap(err, "new uniswap pair caller")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	reserves, err := caller.GetReserves(&bind.CallOpts{
+		Context:     ctx,
+		BlockNumber: big.NewInt(bn),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "get reserves")
+	}
+	// This is the tricky bit.
+	// The reserve call returns the reserves for token0 and token1 in a sorted order.
+	// This means we need to check if our token addresses are sorted or not and flip the reserves if they are not sorted.
+	stoken0, _ := sortAddressess(token0, token1)
+	if stoken0 != token0 {
+		// We're not sorted, so the reserves need to be flipped to represent the actual reserves.
+		reserves.Reserve0, reserves.Reserve1 = reserves.Reserve1, reserves.Reserve0
+	}
+	return &Reserve{Reserve0: reserves.Reserve0, Reserve1: reserves.Reserve1, BlockTimestampLast: reserves.BlockTimestampLast}, nil
+}
+
+func (c *Client) GetTokens(addr common.Address) ([]common.Address, error) {
+	caller, err := uniswapv2pair.NewUniswapv2pairCaller(addr, c.bc)
+	if err != nil {
+		return nil, errors.Wrap(err, "new uniswap pair caller")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	token0, err := caller.Token0(&bind.CallOpts{
+		Context: ctx,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "get token0 failed")
+	}
+	token1, err := caller.Token1(&bind.CallOpts{
+		Context: ctx,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "get token1 failed")
+	}
+	return []common.Address{token0, token1}, nil
+}
+
 // GetExchangeAmount returns the amount of tokens you'd receive when exchanging the given amount of token0 to token1.
 func (c *Client) GetExchangeAmount(amount *big.Int, token0, token1 common.Address) (*big.Int, error) {
 	reserves, err := c.GetReserves(token0, token1)
